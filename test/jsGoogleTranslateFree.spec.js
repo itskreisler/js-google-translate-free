819b854de012a0bca3418d10cb19e5d9d4c00a9f
Kreisler Ramirez <itskreisler@outlook.com>
2023-05-29 16:53:31 -0500

    new version

--- test/jsGoogleTranslateFree.spec.js
+++ test/jsGoogleTranslateFree.spec.js
@@ -5,8 +5,33 @@
 
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT NATIVE NODE MODULES
-const { describe, it, afterEach, beforeEach } = require('node:test')
+const { describe, it } = require('node:test')
 const assert = require('node:assert')
 
 // » IMPORT MODULES
 const jsGoogleTranslateFree = require('..')
+
+// ━━ TEST ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+describe('jsGoogleTranslateFree', async () => {
+  it('should return a string', async () => {
+    const source = 'es'
+    const target = 'en'
+    const text = 'buenos días'
+    const result = await jsGoogleTranslateFree.translate(source, target, text)
+    assert.strictEqual(typeof result, 'string')
+  })
+  it('should return a translation', async () => {
+    const source = 'es'
+    const target = 'en'
+    const text = 'buenos días'
+    const result = await jsGoogleTranslateFree.translate(source, target, text)
+    assert.strictEqual(result, 'good morning')
+  })
+  it('should return a translation', async () => {
+    const source = 'es'
+    const target = 'en'
+    const text = 'no tengas un buen dia, ten un gran dia!'
+    const result = await jsGoogleTranslateFree.translate(source, target, text)
+    assert.strictEqual(result, "Don't have a good day, have a great day!")
+  })
+})
